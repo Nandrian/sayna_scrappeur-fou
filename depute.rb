4ca86b607819b7c359d09124d1544eae6bb0f18f
Nandrian <ranarkai@gmail.com>
2020-11-19 09:29:38 +0300

    Add URI.open

--- depute.rb
+++ depute.rb
@@ -6,7 +6,7 @@ def deputer_email(url,nom)
 	compteur2 = 0
 	deputes = Hash.new
 	url.each{|x|
-		doc2 = Nokogiri::HTML(open(x))
+		doc2 = Nokogiri::HTML(URI.open(x))
 		email_tab[compteur] =  doc2.css('dl.vcard.dep-fiche  dd:nth-child(4) > a:nth-child(1)')
 		deputes[nom[compteur2]] = email_tab[compteur].text
 		puts deputes 
@@ -23,7 +23,7 @@ def depute_url
 	nom_tab = []
 	compteur = 0
 	compteur2 = 0
-	deputes = Nokogiri::HTML(open("http://www2.assemblee-nationale.fr/deputes/liste/alphabetique"))
+	deputes = Nokogiri::HTML(URI.open("http://www2.assemblee-nationale.fr/deputes/liste/alphabetique"))
 	deputes.css('div#deputes-list ul  li  a').each do |depute_name|
 		depute_tab[compteur] = "http://www2.assemblee-nationale.fr#{depute_name['href']}"
 		compteur += 1
